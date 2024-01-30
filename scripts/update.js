c1308d604db85988bdc0c23231c957c314e8b6be
LightHostingFree <141650265+LightHostingFree@users.noreply.github.com>
2024-01-30 18:47:43 +0530

    Update update.js

--- scripts/update.js
+++ scripts/update.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 const path = require("path");
 
 const directoryPath = path.join(__dirname, "../../domains");
-const directoryPath2 = path.join(__dirname, "../../domains/AorzoHosting");
 
 let combinedArray = [];
 
