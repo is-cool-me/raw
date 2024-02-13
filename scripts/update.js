a93ec53cba58a592b4b4271a40950f79053b38f2
LightHostingFree <141650265+LightHostingFree@users.noreply.github.com>
2024-02-13 07:27:37 +0530

    Update update.js

--- scripts/update.js
+++ scripts/update.js
@@ -1,43 +1,48 @@
 const fs = require("fs");
 const path = require("path");
 
-const directoryPath = path.join(__dirname, "../../domains");
+const directoryPaths = [
+    path.join(__dirname, "../../domains"),
+    path.join(__dirname, "../../domains/AorzoHosting")
+];
 
 let combinedArray = [];
 
-fs.readdir(directoryPath, function (err, files) {
-    if (err) throw err;
+directoryPaths.forEach(function(directoryPath) {
+    fs.readdir(directoryPath, function (err, files) {
+        if (err) throw err;
 
-    function removeValue(value, index, arr) {
-        if (value === "reserved") {
-            arr.splice(index, 1);
-            return true;
+        function removeValue(value, index, arr) {
+            if (value === "reserved") {
+                arr.splice(index, 1);
+                return true;
+            }
+
+            return false;
         }
 
-        return false;
-    }
+        files.filter(removeValue);
 
-    files.filter(removeValue);
+        files.forEach(function (file) {
+            const filePath = path.join(directoryPath, file);
 
-    files.forEach(function (file) {
-        const filePath = path.join(directoryPath, file);
+            fs.readFile(filePath, "utf8", (err, data) => {
+                if (err) throw err;
 
-        fs.readFile(filePath, "utf8", (err, data) => {
-            if (err) throw err;
+                const dataArray = [JSON.parse(data)];
 
-            const dataArray = [JSON.parse(data)];
+                for (const item of dataArray) {
+                    item.owner.email = item.owner.email.replace(/@/, " (at) ");
+                }
 
-            for (const item of dataArray) {
-                item.owner.email = item.owner.email.replace(/@/, " (at) ");
-            }
+                combinedArray = combinedArray.concat(dataArray);
 
-            combinedArray = combinedArray.concat(dataArray);
-
-            if (combinedArray.length === files.length) {
-                fs.writeFile("raw/index.json", JSON.stringify(combinedArray), (err) => {
-                    if (err) throw err;
-                });
-            }
+                if (combinedArray.length === files.length) {
+                    fs.writeFile("raw/index.json", JSON.stringify(combinedArray), (err) => {
+                        if (err) throw err;
+                    });
+                }
+            });
         });
     });
 });
