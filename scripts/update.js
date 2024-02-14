f6497619bf9ab0f5369017c0661e050bb9da0831
LightHostingFree <141650265+LightHostingFree@users.noreply.github.com>
2024-02-14 09:55:56 +0530

    Update update.js

--- scripts/update.js
+++ scripts/update.js
@@ -22,21 +22,29 @@ fs.readdir(directoryPath, function (err, files) {
     files.forEach(function (file) {
         const filePath = path.join(directoryPath, file);
 
-        fs.readFile(filePath, "utf8", (err, data) => {
+        fs.stat(filePath, (err, stats) => {
             if (err) throw err;
 
-            const dataArray = [JSON.parse(data)];
-
-            for (const item of dataArray) {
-                item.owner.email = item.owner.email.replace(/@/, " (at) ");
-            }
-
-            combinedArray = combinedArray.concat(dataArray);
-
-            if (combinedArray.length === files.length) {
-                fs.writeFile("raw/index.json", JSON.stringify(combinedArray), (err) => {
+            if (stats.isFile()) {
+                fs.readFile(filePath, "utf8", (err, data) => {
                     if (err) throw err;
+
+                    const dataArray = [JSON.parse(data)];
+
+                    for (const item of dataArray) {
+                        item.owner.email = item.owner.email.replace(/@/, " (at) ");
+                    }
+
+                    combinedArray = combinedArray.concat(dataArray);
+
+                    if (combinedArray.length === files.length) {
+                        fs.writeFile("raw/index.json", JSON.stringify(combinedArray), (err) => {
+                            if (err) throw err;
+                        });
+                    }
                 });
+            } else {
+                console.log(filePath + " is a directory. Skipping...");
             }
         });
     });
